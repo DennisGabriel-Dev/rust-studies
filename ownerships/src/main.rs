3830e122c1c2c92fd1e2b00cae8f67623c48b9bc
DennisGabriel <dennisgabr745@gmail.com>
2025-01-02 23:59:23 -0300

    add ownerships examples

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello"); // this format is a mutable string, rather using literals doesn´t  
     s.push_str(", world!"); // concat text
     println!("{:?}", s); //OUTPUT: hello, world!
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    //println!("{s1}"); //ERROR! because s1 isn't available in this scope anymore
 }

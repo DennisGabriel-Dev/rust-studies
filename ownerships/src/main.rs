25bab5a5759c37cc8af532be0862ddc712d5094a
DennisGabriel <dennisgabr745@gmail.com>
2025-01-03 23:36:37 -0300

    clone example in ownerships

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     println!("{:?}", s); //OUTPUT: hello, world!
 
     let s1 = String::from("hello");
-    let s2 = s1;
+    //let s2 = s1;
     //println!("{s1}"); //ERROR! because s1 isn't available in this scope anymore
+    let s2 = s1.clone();
+    println!("s1 = {s1}, s2 = {s2}"); // in this case s1 is available, the clone method makes a copy by s1 value 
 }
